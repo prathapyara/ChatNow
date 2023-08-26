3737de79dbf7fc853bc142b8cbf48610839b5b8d
yara_ prathap_22768238 <prathapyara@gmaail.com>
2023-08-26 12:06:56 +0530

    chatnow

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { checkUser,generateToken,getAllUsers,onBoardUser } from "../controllers/AuthController.js";
+import { checkUser,generateToken,getAllUsers,onBoardUser} from "../controllers/AuthController.js";
 
 const router=Router();
 
@@ -7,5 +7,4 @@ router.post("/check-user",checkUser);
 router.post("/onboard-user",onBoardUser);
 router.get("/get-contacts",getAllUsers);
 router.get("/generate-token/:userId",generateToken);
-
 export default router;
